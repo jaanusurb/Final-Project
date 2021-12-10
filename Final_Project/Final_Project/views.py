d29bda3e69dd1a01d05009945b243ebdccb0ca18
jaanusurb <jaanus.urb@live.com>
2021-12-11 01:23:18 +0200

    welcome Indrek Jaanus kertu-Liis

--- Final_Project/Final_Project/views.py
+++ Final_Project/Final_Project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    request.session['name'] = 'Indrek'
+    request.session['name'] = 'Indrek, Jaanus, Kertu-Liis'
     username = request.session['name']
 
     return render(request, 'home.html', context={'username': username})
