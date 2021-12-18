5d3edd92fc0bf967d68835996e8f619dc39f54f2
KuLsKa <kerttuliis.kaljuma@gmail.com>
2021-12-18 12:28:31 +0200

    Few changes from yesterday. Let's try, can I push it to Github.

--- Final_Project/Final_Project/views.py
+++ Final_Project/Final_Project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    request.session['name'] = 'Indrek'
+    request.session['name'] = 'User'
     username = request.session['name']
 
     return render(request, 'home.html', context={'username': username})
